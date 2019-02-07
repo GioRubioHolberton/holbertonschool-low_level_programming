9017122b4f484ded56390aa99487134e1b6f58ab
GioRubioHolberton <759@holbertonschool.com>
2019-02-07 03:48:42 +0000

    Add solution for exercise 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *Print with puts funtion
  *Retourn: 0
  */
@@ -8,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
