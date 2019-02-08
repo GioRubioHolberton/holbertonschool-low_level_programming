4f1ceec0f41bec7f9588cb218569fa9b7b3440c9
GioRubioHolberton <759@holbertonschool.com>
2019-02-08 23:30:53 +0000

    Add solution exercise 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - principal funtion
+ * Determine the last digit of a number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+		printf("%d", n);
+		putchar('\n');
+
+	return (0);
+
+}
