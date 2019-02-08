2208ec4a637fd94dab58b201081e95f55bcb04e6
GioRubioHolberton <759@holbertonschool.com>
2019-02-08 23:11:45 +0000

    Add solution exercise 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - principal funtion
+ * Determine the last digit of a number
+ * Return: 0
+ */
+int main(void)
+{
+	int abc;
+
+	for (abc = 97 ; abc <= 122 ; abc++)
+		putchar(abc);
+	for (abc = 65 ; abc <= 90 ; abc++)
+		putchar(abc);
+
+	putchar('\n');
+
+	return (0);
+
+}
