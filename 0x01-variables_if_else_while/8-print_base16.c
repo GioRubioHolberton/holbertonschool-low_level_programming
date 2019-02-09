accf45f66a2ae1d5686a89acd2ebce08c24bdc75
GioRubioHolberton <759@holbertonschool.com>
2019-02-09 00:57:20 +0000

    Add solution exercise 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - principal funtion
+ * Determine the last digit of a number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+		putchar(n);
+	for (n = 97 ; n <= 102 ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
