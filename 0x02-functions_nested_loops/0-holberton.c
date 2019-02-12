e969b4eab58de6a7e5ade9bb031f18e0eb0e18bd
GioRubioHolberton <759@holbertonschool.com>
2019-02-12 20:07:28 +0000

    Add solution for exercise 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Program that prints Holberton
+ *
+ * Return: 0 Succsess
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
