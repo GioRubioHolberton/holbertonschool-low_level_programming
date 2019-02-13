8b35f297765894c553bfd6ada45736badaa63ee8
GioRubioHolberton <759@holbertonschool.com>
2019-02-13 04:01:56 +0000

    Add solution for exercise 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - prints the last digit of a number..
+ *
+ * @a: variable
+ * @b: variable
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+  * main - principal funtion
+  * Determine the last digit of a number
+  * Return: 0
+  */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+	for (a = 48 ; a <= 50 ; a++)
+	{
+		for (b = 48 ; b <= 57 ; b++)
+		{
+			if (b != )
+			for (c = 48 ; c <= 53 ; c++)
+			{
+				for (d = 48 ; d <= 57 ; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+
+		}
+	}
+}
