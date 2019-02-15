23d1d24678d3074ce2c57ed6d5468e6fd289a432
GioRubioHolberton <759@holbertonschool.com>
2019-02-15 02:23:09 +0000

    Add solution for exercise 6

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line -  function that draws a straight line in the terminal.
+ *
+ * @n : Variable
+ *
+ */
+void print_line(int n)
+
+{
+	int o;
+
+	if (n > 0)
+	{
+		for (o = 0 ; o <= n ; o++)
+		_putchar ('_');
+	}
+	_putchar ('\n');
+}
