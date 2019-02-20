533ea5f20c18824ba5eec6e4780247bbbe1db3d0
GioRubioHolberton <759@holbertonschool.com>
2019-02-20 22:58:54 +0000

    Add solution for exercise 5

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - function that reverses a string.
+ *
+ * @s: Pointer
+ *
+ * Return: value on l
+ */
+void rev_string(char *s)
+{
+	int star, end, aux;
+
+	star = 0;
+	end = 0;
+	while (*(s + end))
+	{
+		end++;
+	}
+
+	end--;
+
+	while (star != end || star < end)
+	{
+		aux = s[end];
+		s[end] = s[star];
+		s[star] = aux;
+		star++;
+		end--;
+	}
+
+}
