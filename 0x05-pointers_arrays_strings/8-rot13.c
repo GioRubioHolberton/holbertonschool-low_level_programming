4b86560bb8f1db082ffdda32db054beee41b0e5c
GioRubioHolberton <759@holbertonschool.com>
2019-02-22 22:33:51 +0000

    Add solution for exercise 8

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+  * *rot13 - function that encodes a string into 1337.
+  *
+  * @s: pointer
+  *
+  *Return: dest
+  */
+char *rot13(char *s)
+{
+	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char change[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int cont = 0, iter;
+
+	while (s[cont])
+	{
+		iter = 0;
+		while (normal[iter])
+		{
+			if (s[cont] == normal[iter])
+			{
+				s[cont] = change[iter];
+				break;
+			}
+			iter++;
+		}
+		cont++;
+	}
+	return (s);
+}
