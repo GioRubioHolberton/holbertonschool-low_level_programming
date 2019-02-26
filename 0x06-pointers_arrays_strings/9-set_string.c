eb58cbd411153f781b92d289434b417cb8c2c395
GioRubioHolberton <759@holbertonschool.com>
2019-02-26 03:04:36 +0000

    Add solution for exercise 8

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * set_string - function that locates a character in a string.
+ *
+ * @s: pointer
+ * @to: pointer
+ *
+ * Return: s
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
