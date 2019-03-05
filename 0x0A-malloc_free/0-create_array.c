b7db6cde20707471747cee67f6b17ae53cef347b
GioRubioHolberton <759@holbertonschool.com>
2019-03-05 21:12:01 +0000

    Add solution for exercise 0

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ *create_array - Write a program that adds positive numbers.
+ *
+ *@size:  int 1
+ *@c: pointer
+ *Return: NULL or c
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int con;
+	char *a;
+
+	if (size == 0)
+		return ("NULL");
+
+	a = malloc(size * sizeof(char));
+
+	if (!a)
+		return ("NULL");
+
+	for (con = 0 ; con < size ; con++)
+	{
+		a[con] = c;
+	}
+	return (a);
+}
