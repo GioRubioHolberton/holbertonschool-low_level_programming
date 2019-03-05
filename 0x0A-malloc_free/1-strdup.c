e777411a37ee6e66a255a7783ab738fbfb4d21af
GioRubioHolberton <759@holbertonschool.com>
2019-03-05 23:44:17 +0000

    Add solution for exercise 1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ *_strdup - Write a program that adds positive numbers.
+ *
+ *@str:  int 1
+ *Return: NULL or r
+ */
+char *_strdup(char *str)
+{
+	int con, iter;
+	char *r;
+
+	con = 0;
+	iter = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[con])
+	{
+		con++;
+	}
+
+	r = malloc(con * sizeof(char));
+
+	while (iter <= con + 1)
+	{
+		r[iter] = str[iter];
+		iter++;
+
+	}
+	return (r);
+}
