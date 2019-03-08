1d08da0841fc75d89e238f330dbf060e4bbacfc2
GioRubioHolberton <759@holbertonschool.com>
2019-03-08 03:50:26 +0000

    Add solution for exercise 1

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ *string_nconcat - Write a program that adds positive numbers.
+ *
+ *@s1: char
+ *@s2: char
+ *@n: unsigned int
+ *Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *plop;
+	unsigned int con1, con2, iter1, iter2;
+
+	con1 = 0;
+	con2 = 0;
+	iter1 = 0;
+	iter2 = 0;
+
+	while (s1[con1])
+		con1++;
+
+	while (s2[con2])
+		con2++;
+
+	if (n > con2)
+		n = con2;
+
+	plop = malloc(sizeof(char) * (con1 + n));
+
+	if (!plop)
+		return (NULL);
+
+	while (iter1 < con1)
+	{
+		plop[iter1] = s1[iter1];
+		iter1++;
+	}
+
+	while (iter2 < n)
+	{
+		plop[iter1 + iter2] = s2[iter2];
+		iter2++;
+	}
+
+	/*plop[iter1 + iter2] = NULL;*/
+
+	return (plop);
+}
