28d23922114d42b545617d3f1e80ca28e964dff9
GioRubioHolberton <759@holbertonschool.com>
2019-03-12 02:57:36 +0000

    Add solution for exercise 0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MY_DOG
+#define MY_DOG
+
+/**
+ *struct dog - type struct
+ *@name: variable char
+ *@age: variable float
+ *@owner: variable char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
