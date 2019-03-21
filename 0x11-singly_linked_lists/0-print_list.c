eb0411a17e27c948f2d31aa251dc09590353b06f
GioRubioHolberton <759@holbertonschool.com>
2019-03-21 23:45:53 +0000

    Add solution for exercise 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int coun;
+
+	for (coun = 0 ; h != NULL ; coun++)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+
+	}
+
+	return (coun);
+}
