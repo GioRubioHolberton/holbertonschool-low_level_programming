9a92ea3d17d82a23a1aabc3d8f84d3dca8a4f8d3
GioRubioHolberton <759@holbertonschool.com>
2019-03-22 00:52:19 +0000

    Add solution for exercise 5

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * Tortoise - function that prints all the elements of a list_t list.
+ *
+ * Return: the number of nodes
+ */
+void Tortoise(void) __attribute__ ((constructor));
+void Tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
