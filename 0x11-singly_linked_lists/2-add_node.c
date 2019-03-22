0f947921bc23e810313bbd074ae2388741e3bc98
GioRubioHolberton <759@holbertonschool.com>
2019-03-22 04:07:10 +0000

    Add new solution for exercise 2, correction for strlen

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -4,36 +4,34 @@
 #include <string.h>
 
 /**
- * print_list - function that prints all the elements of a list_t list.
- * @h: list
- *
- * Return: the number of nodes
+ * add_node - function that prints all the elements of a list_t list.
+ * @str: list
+ * @head: 1st node
+ * Return: my nodo in head
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	int coun;
+	int coun = 0;
 
 	list_t *my_node;
 
 	my_node = malloc(sizeof(list_t));
 
 	if (my_node == NULL)
+	{
+		free(my_node);
 		return (NULL);
+	}
 
 	my_node->str = strdup(str);
 
-	if (my_node->str == NULL)
-		free(my_node);
-		return (NULL);
-
-	for (coun = 0 ; my_node->str != '\0' ; coun++)
-	{
-	}
+	while (str[coun] != '\0')
+		coun++;
 
 	my_node->len = coun;
 	my_node->next = *head;
-	my_node = *head;
+	*head = my_node;
 
-	return (my_node);
+	return (*head);
 }
