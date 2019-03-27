be53b643efeac18170356cb75a335da6e9b95ec8
GioRubioHolberton <759@holbertonschool.com>
2019-03-27 04:46:25 +0000

    Add solution for exercise 6

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - function that frees a listint_t list.
+ *
+ * @head: Pointer to list
+ * Return: n
+ */
+
+int pop_listint(listint_t **head)
+{
+	int otrn;
+	listint_t *otrh;
+
+	if (head != NULL)
+	{
+		otrn = (*head)->n;
+		otrh = (*head)->next;
+		free(*head);
+		*head = otrh;
+	}
+
+	return (otrn);
+}
