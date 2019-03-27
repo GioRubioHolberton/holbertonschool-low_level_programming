e0aaed0b8e8b8c69709e8cd2fb28eb8800c644bb
GioRubioHolberton <759@holbertonschool.com>
2019-03-27 03:59:42 +0000

    Add solution for exercise 5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ *
+ * @head: Pointer to 1st node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *otr;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			otr = *head;
+			free(otr);
+			*head = otr;
+		}
+	}
+}
