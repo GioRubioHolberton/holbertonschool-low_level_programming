820a8c19840bad66860d30d3fe1f3363b3579073
GioRubioHolberton <759@holbertonschool.com>
2019-03-29 02:23:46 +0000

    Add solution for exercise 0 with betty style

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -3,9 +3,9 @@
 
 /**
  * binary_to_uint - function that prints all the elements of a list_t list.
- * @h: list
+ * @b: string
  *
- * Return: the number of nodes
+ * Return: unsigned integer number
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -15,10 +15,10 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (coun = 0 ; b[coun] != '\0' ; coun++)
-		{
-			if (b[coun] != '0' && b[coun] != '1')
-				return (0);
+	for (coun = 0 ; b[coun] != '\0'; coun++)
+	{
+		if (b[coun] != '0' && b[coun] != '1')
+			return (0);
 		num = num << 1;
 		num = num | (b[coun] - '0');
 	}
