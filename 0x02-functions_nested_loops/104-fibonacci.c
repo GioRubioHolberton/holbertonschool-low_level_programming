e35c7533845ba6760b01680773d4bd2d972f3820
GioRubioHolberton <759@holbertonschool.com>
2019-04-05 15:58:43 +0000

    Add solution for exercise 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - principal funtion
+ * prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int f = 1, aux = 0, ot = 0;
+
+	for (i = 0 ; i <= 98 ; i++)
+	{
+		f = f + aux;
+		aux = ot;
+		ot = f;
+		if (i != 0)
+		{
+			printf("%lu", f);
+			if (i != 50)
+				printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
