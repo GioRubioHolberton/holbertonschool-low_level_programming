b946766aef0a8a4f5578173368fe1f7c841efb17
GioRubioHolberton <759@holbertonschool.com>
2019-04-07 01:23:10 +0000

    Add solution for exercise 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -6,36 +6,48 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * create_file - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * strlen - len
+ * @str: string
+ * Return: s
+ */
+int str_len(char *s)
+{
+	int sl = 0;
+	while (*s != 0)
+	{
+		sl++;
+		s++;
+	}
+	return (sl);
+}
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to print
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print.
  *
  */
 int create_file(const char *filename, char *text_content)
 {
 	int st;
 	int fd;
-	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
-	{
-		close(fd);
 		return (-1);
-	}
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-	while ((text_content + len) != '\0')
-		len++;
-
-	st = write(fd, text_content, len(text_content));
+	st = write(fd, text_content, str_len(text_content));
 	if (st < 0)
 		return (-1);
+	
 	return (1);
+
 }
