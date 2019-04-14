7c7927a46ee03a67e7fc63252cf8e6138ea076ab
GioRubioHolberton <759@holbertonschool.com>
2019-04-14 20:30:00 +0000

    Add solution for exercise 12

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,34 @@
 #include "holberton.h"
 
 /**
- * print_triangle -  function that prints a triangle
+ * print_number -  function that prints a triangle
  *
- * @size : Variable
+ * @n : number to print
  *
  */
 void print_number(int n)
 {
-    int m, o;
-    if (n < 0) {
-        n = n *(-1);
-        _putchar('-');
-    }
-    if (n / 1000 != 0)
-        _putchar(n / 1000 + '0');
-    m = n % 1000;
-    if (n / 100 != 0)
-        _putchar(m / 100 + '0');
-    o = m % 100;
-    if (n / 10 != 0)
-        _putchar(o / 10 + '0');
-    _putchar(o % 10 + '0');
+	unsigned int i, j, aux, m, d = 1;
+
+	aux = n;
+	m = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		aux = -aux;
+		m = -m;
+	}
+	for (i = 0 ; aux / 10 > 0 ; i++)
+	{
+		aux = aux / 10;
+		d = d * 10;
+	}
+
+	for (j = i ; j > 0 ; j--)
+	{
+		_putchar(m / d + '0');
+		m = m % d;
+		d = d / 10;
+	}
+	_putchar(m % 10 + '0');
 }
