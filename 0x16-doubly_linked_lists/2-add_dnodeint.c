aac23ff3e40667a2ab9b063b572236ded41d731b
GioRubioHolberton <759@holbertonschool.com>
2019-05-06 00:41:53 +0000

    Add node

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *add_dnodeint - function that returns the number of elements in a linked list.
+ *@head: header
+ *@n: numbers
+ *Return: number of elements
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *add = malloc(sizeof(dlistint_t));
+
+	if (!add)
+		return (all);
+	add->n = n;
+	add->next = *head;
+	add->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = add;
+	*head = add;
+	return (add);
+}
