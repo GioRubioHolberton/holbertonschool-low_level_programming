5648188788429d2ecbba5c601d8db16365c66962
GioRubioHolberton <759@holbertonschool.com>
2019-07-26 15:48:23 +0000

    correction conditianals for NULL

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -6,21 +6,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t my_left = 1, my_right = 1;
+	size_t my_left = 0, my_right = 0;
 
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL)
-		return (0);
-	if (tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	my_left += binary_tree_height(tree->left);
-	my_right += binary_tree_height(tree->right);
+	my_left = binary_tree_height(tree->left);
+	my_right = binary_tree_height(tree->right);
 
 	if (my_left < my_right)
-		return (my_right);
+		return (my_right + 1);
 	else
-		return (my_left);
+		return (my_left + 1);
 }
