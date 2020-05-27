94d2683290414f917d299c5423f34fd4daa889f2
GioRubioHolberton <759@holbertonschool.com>
2020-05-27 00:45:22 +0000

    use float

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i;
-	long unsigned int f = 1, aux = 0, ot = 0;
+	float f = 1, aux = 0, ot = 0;
 
 	for (i = 0 ; i <= 98 ; i++)
 	{
@@ -16,7 +16,7 @@ int main(void)
 		ot = f;
 		if (i != 0)
 		{
-			printf("%lu", f);
+			printf("%0.f", f);
 			if (i != 98)
 				printf(", ");
 		}
